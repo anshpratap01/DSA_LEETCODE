1e49dcb033dcea84b684bf0df8d48f0533bb919f
Ansh Pratap <anshpratap866@gmail.com>
2025-12-01 22:15:32 +0530

    Time: 27 ms (20.88%) | Memory: 72.3 MB (50.55%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long maxRunTime(int n, int[] batteries) {
+        long sum = 0;
+        for (int battery : batteries) {
+            sum += battery;
+        }
+        
+        Arrays.sort(batteries);
+        
+        for (int i = batteries.length - 1; i >= 0; i--) {
+            if (batteries[i] > sum / n) {
+                sum -= batteries[i];
+                n--;
+            } else {
+                return sum / n;
+            }
+        }
+        return sum / n;
+        
+    }
+}
