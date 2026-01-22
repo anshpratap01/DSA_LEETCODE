09844cfb0387bfe4a93af4d0be4a8dbbcb8af96c
Ansh Pratap <anshpratap866@gmail.com>
2026-01-23 00:17:53 +0530

    Time: 2 ms (63.70%) | Memory: 43.4 MB (26.67%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,23 +1,19 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        
-        char c[] = s.toCharArray();
-        char d[] = t.toCharArray();
-        Arrays.sort(c);
-        Arrays.sort(d);
+        int xor1 = 0;
+        for(int i = 0; i<t.length(); i++){
+            xor1 = xor1^(t.charAt(i));
 
-       
-        
-        for(int i = 0; i<c.length; i++){
-            if(c[i] != d[i]){
-                return d[i];
-            }
-
-           
-            
         }
-        int n = d.length;
-        return d[n-1];
+        
+        for(int i = 0; i<s.length(); i++){
+            xor1 = xor1^(s.charAt(i));
+
+        }
+
+        return (char)xor1;
+
+
         
     }
 }
