8f626643fcd46b16ecc6bfc40408b86e38336d04
Ansh Pratap <anshpratap866@gmail.com>
2026-02-05 10:53:34 +0530

    Time: 79 ms (70.75%) | Memory: 82.4 MB (31.05%) - LeetSync

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
@@ -1,21 +1,24 @@
 class Solution {
-    public boolean isPossibleDivide(int[] hand, int k) {
-        int n = hand.length;
+    public boolean isPossibleDivide(int[] nums, int k) {
+        int n = nums.length;
         if(n%k != 0) return false;
         HashMap<Integer,Integer> mp = new HashMap<>();
-        for(int num : hand){
-            mp.put(num,1+mp.getOrDefault(num,0));
+        for(int num : nums){
+            mp.put(num , 1+ mp.getOrDefault(num,0));
         }
-        Arrays.sort(hand);
 
-        for(int num : hand){
-            if(mp.get(num) == 0) continue;
-            for(int i = 0; i<k; i++){
+       Arrays.sort(nums);
+
+        for(int num : nums){
+             if(mp.get(num) == 0) continue;
+           
+            for(int i = 0; i<k;i++){
                 int curr = num+i;
                 if(!mp.containsKey(curr) || mp.get(curr) == 0){
                     return false;
                 }
-                mp.put(curr,mp.get(curr)-1);
+                mp.put(curr,mp.get(curr) -1);
+                
 
             }
         }
