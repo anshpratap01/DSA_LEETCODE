8cca909df1e269013c8662e0fee6e9675bd7bbd5
Ansh Pratap <anshpratap866@gmail.com>
2026-02-13 08:02:18 +0530

    Time: 1 ms (94.75%) | Memory: 43.6 MB (50.66%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,21 +1,22 @@
 class Solution {
     public List<List<Integer>> generate(int n) {
-        List<List<Integer>>ans = new ArrayList<>();
+        // pasacal Triangle New Solution
+        List<List<Integer>> ans = new ArrayList<>();
 
         for(int i = 0; i<n; i++){
-            int level = 1;
-            List<Integer> a = new ArrayList<>();
-            a.add(level);
-            for(int j = 1; j<=i; j++){
-                level = level*(i-j+1);
-                level = level/j;
-                a.add(level);
+            ans.add(new ArrayList<>());
+            for(int  j = 0; j<=i; j++){
+                if(j == 0 || j == i){
+                    ans.get(i).add(1);
+                }else{
+                    int val = ans.get(i-1).get(j) + ans.get(i-1).get(j-1);
+                    ans.get(i).add(val);
+                }
 
             }
-            ans.add(a);
+
         }
         return ans;
-
         
     }
 }
