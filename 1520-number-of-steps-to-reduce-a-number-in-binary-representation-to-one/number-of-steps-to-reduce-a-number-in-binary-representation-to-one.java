c4e196c33300852a344c851bb5d489ed9a4ce701
Ansh Pratap <anshpratap866@gmail.com>
2026-02-26 07:10:46 +0530

    Time: 4 ms (21.83%) | Memory: 45.4 MB (19.47%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -13,15 +13,18 @@ class Solution {
             else {
                 int i = sb.length() - 1;
 
-                
+                // jab tak 0 add kro jab tak 1 hai bss index dekh lo kahi -1 m toh nhi gya
                 while (i >= 0 && sb.charAt(i) == '1') {
                     sb.setCharAt(i, '0');
                     i--;
                 }
-
+                // carry bachi
+                // ab bacha ki ab toh zero hi ayega 
+                // jb pahela zero ayega usko usme add kr do
                 if (i >= 0) {
                     sb.setCharAt(i, '1');
                 } else {
+                    // ya ab zero hai hi nhi toh carry ko append kr do
                     sb.insert(0, '1');
                 }
             }
