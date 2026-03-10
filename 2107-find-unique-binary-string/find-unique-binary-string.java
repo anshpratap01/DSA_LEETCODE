644fb88cf020610b227d01802be8c4246f2e3d7d
Ansh Pratap <anshpratap866@gmail.com>
2026-03-10 23:08:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (36.31%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -3,8 +3,14 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         for (int i = 0; i < nums.length; i++) {
-            sb.append(nums[i].charAt(i) == '0' ? '1' : '0'); 
+            if(nums[i].charAt(i) == '0'){
+                sb.append('1');
+            }else{
+                sb.append('0');
+            }
+            // sb.append(nums[i].charAt(i) == '0' ? '1' : '0'); 
         }
+       // System.out.print(sb);
 
         return sb.toString(); 
         
