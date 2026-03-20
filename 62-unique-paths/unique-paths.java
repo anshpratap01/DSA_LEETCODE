0a4733d48c9bba966f1aade88fae6b625848c982
Ansh Pratap <anshpratap866@gmail.com>
2026-03-20 10:19:14 +0530

    Time: 1 ms (14.09%) | Memory: 42.1 MB (77.20%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,35 +1,25 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-         int board[][] = new int[m][n];
-        // 0 means not visited
-        // 1 means visitedboard
-
-        int dp[][] = new int [m][n];
-        for(int arr[] :dp){
-            Arrays.fill(arr,-1);
-        }
-        return helper(0,0,board,dp);
-        
-    }
-
-    public int helper(int i, int j, int board[][], int dp[][]){
-        if(i>=board.length || j>=board[0].length || board[i][j]==1){
-            return 0;
-        }
-        if(i==board.length-1 && j==board[0].length-1){
+    int dp[][];
+    public int solve(int m , int n){
+        if(m == 0 || n == 0){
             return 1;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+        if(dp[m][n] != -1){
+            return dp[m][n];
         }
 
-        board[i][j]=1;
-        int ans =0;
-        ans+=helper(i,j+1,board,dp);
-        ans+=helper(i+1,j,board,dp);
-        board[i][j]=0;
-
-        return dp[i][j]=ans;
+        dp[m][n] =  solve(m-1, n) + solve(m, n-1);
+        return dp[m][n];
+    }
+    public int uniquePaths(int m, int n) {
+        dp = new int[m+1][n+1];
+        for(int i = 0; i<=m; i++){
+            for(int j = 0; j<=n;j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        return solve(m-1, n-1);
         
     }
 }
