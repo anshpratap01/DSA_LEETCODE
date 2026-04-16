99e21c79d736ee0de9dc257f68ad785ecc98016c
Ansh Pratap <anshpratap866@gmail.com>
2026-04-16 22:31:00 +0530

    Time: 8 ms (74.11%) | Memory: 65.3 MB (43.96%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,41 +1,32 @@
 class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        dfs(s, 0, new ArrayList<>(), res);
+        return res;
+    }
 
-    List<List<String>> res;
+    private void dfs(String s, int start, List<String> path, List<List<String>> res) {
+        if (start == s.length()) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = start; i < s.length(); i++) {
+            if (isPalindrome(s, start, i)) {
+                path.add(s.substring(start, i + 1));
+                dfs(s, i + 1, path, res);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
 
-    public boolean ispalindrom(String s){
-        int start = 0;
-        int end = s.length() - 1;
-
-        while(start < end){
-            if(s.charAt(start) != s.charAt(end)){
+    private boolean isPalindrome(String s, int start, int end) {
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
                 return false;
             }
             start++;
             end--;
         }
         return true;
-    }
-
-    public void solve(String s, int idx, List<String> ans){
-        if(idx == s.length()){
-            res.add(new ArrayList<>(ans));
-            return;
-        }
-
-        for(int i = idx; i < s.length(); i++){
-            String sub = s.substring(idx, i+1);
-
-            if(ispalindrom(sub)){
-                ans.add(sub);
-                solve(s, i+1, ans);
-                ans.remove(ans.size()-1);
-            }
-        }
-    }
-
-    public List<List<String>> partition(String s) {
-        res = new ArrayList<>();
-        solve(s, 0, new ArrayList<>());
-        return res;
     }
 }
