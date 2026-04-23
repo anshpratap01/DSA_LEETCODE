eb81a10d3748c6afc775ff0a8b6e4dfaf4bee8e3
Ansh Pratap <anshpratap866@gmail.com>
2026-04-23 11:01:50 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (79.41%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,26 +1,40 @@
 class Solution {
     boolean ans;
-    public void bfs(int i, int[][] graph, int visited[] ){
-        Queue<Integer> q = new LinkedList<>();
-        visited[i] = 0;
-        q.add(i);
-        while(q.size() > 0){
-            int curr = q.poll();
-            int color = visited[curr];
-            for( int ele : graph[curr]){
-                if(visited[ele] == visited[curr]){
+    public void dfs(int i, int[][] graph, int visited[] ){
+        ///Queue<Integer> q = new LinkedList<>();
+        
+       // q.add(i);
+
+       for(int ele : graph[i]){
+       // int color = visited[ele];
+       // same color means graph is not bip
+                if(visited[ele] == visited[i]){
                     ans = false;
                     return;
                 }
                 if(visited[ele] == -1){
-                    visited[ele] = 1- color;
-                    q.add(ele);
+                    visited[ele] = 1- visited[i];
+                   dfs(ele , graph, visited);
                 }
             }
 
-        }
+    //     while(q.size() > 0){
+    //         int curr = q.poll();
+    //         int color = visited[curr];
+    //         for(int ele : graph[curr]){
+    //             if(visited[ele] == visited[curr]){
+    //                 ans = false;
+    //                 return;
+    //             }
+    //             if(visited[ele] == -1){
+    //                 visited[ele] = 1- color;
+    //                 q.add(ele);
+    //             }
+    //         }
 
-    }
+    //     }
+
+     }
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         ans = true;
@@ -30,8 +44,10 @@ class Solution {
         }
 
         for(int i= 0; i<n; i++){
+           
             if(visited[i] == -1){
-                bfs(i , graph, visited);
+                 visited[i] = 0;
+                dfs(i , graph, visited);
             }
         }
         return ans;
