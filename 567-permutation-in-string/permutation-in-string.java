3467ba4000ab1141f39a60eac870b11751992e41
Ansh Pratap <anshpratap866@gmail.com>
2026-09-16 19:54:04 +0530

    Time: 1885 ms (5.08%) | Memory: 47.1 MB (7.66%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,46 +1,55 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        StringBuilder sb = new StringBuilder();
-        int i = 0;
-        int j = 0;
-        while(j<s2.length()){
-            sb.append(s2.charAt(j));
-            if(j-i+1<s1.length()){
-                j++;
-            }else if(j-i+1 == s1.length()){
-                HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
 
-                // frequency of p
-                for (int k = 0; k < s1.length(); k++) {
-                    char c = s1.charAt(k);
-                    map.put(c, map.getOrDefault(c, 0) + 1);
+        
+        StringBuilder sb = new StringBuilder();
+
+ 
+        for( int right = 0; right<s2.length(); right++){
+            sb.append(s2.charAt(right));
+
+            if(right - left+1 == s1.length()){
+             HashMap<Character , Integer> mp = new HashMap<>();
+
+                for(int i = 0; i<s1.length(); i++){
+                    char ch = s1.charAt(i);
+                    mp.put(ch , 1+mp.getOrDefault(ch , 0));
                 }
 
                 boolean flag = true;
 
-                // check window
-                for (int l = 0; l < sb.length(); l++) {
-                    char c = sb.charAt(l);
-                    if (!map.containsKey(c) || map.get(c) == 0) {
+                for(int i = 0; i<sb.length(); i++){
+                    char c = sb.charAt(i);
+                    if(!mp.containsKey(c) || mp.get(c)==0){
                         flag = false;
                         break;
                     }
-                    map.put(c, map.get(c) - 1);
-                }
 
-                if (flag) {
-                    return true;
+                    mp.put(c,mp.get(c)-1);
+
+                    
+                    
+                }
+                if(flag){
+                 return true;
                 }
 
                 sb.deleteCharAt(0);
-                i++;
-                j++;
+                left++;
+
+
+
+
+
             }
-            }
-            return false;
-            
+           
+
+
+
 
         }
-       
+        return false;
         
     }
+}
