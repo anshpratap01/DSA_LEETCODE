44ce81c169dde5346ca95396bab7a2a5c2407898
Ansh Pratap <anshpratap866@gmail.com>
2026-09-21 20:13:43 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (48.65%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -10,21 +10,26 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-         ListNode dummy = new ListNode(0); // created dummy node
-        dummy.next = head;
-        ListNode prev = dummy; // intialising prev pointer on dummy node
+     ListNode dummy = new ListNode(0);
+
+     dummy.next = head;
+    ListNode prev = dummy;
+
+     for(int i = 0;i<left-1; i++){
+        prev = prev.next;
+
+     }
+     ListNode curr = prev.next;
+
+     for(int i = 0; i<right-left ; i++){
+        ListNode forw = curr.next;
+        curr.next = forw.next;
+        forw.next = prev.next;
+        prev.next = forw;
+     }
+
+     return dummy.next;
+
         
-        for(int i = 0; i < left - 1; i++)
-            prev = prev.next; // adjusting the prev pointer on it's actual index
-        
-        ListNode curr = prev.next; // curr pointer will be just after prev
-        // reversing
-        for(int i = 0; i < right - left; i++){
-            ListNode forw = curr.next; // forw pointer will be after curr
-            curr.next = forw.next;
-            forw.next = prev.next;
-            prev.next = forw;
-        }
-        return dummy.next;
     }
 }
